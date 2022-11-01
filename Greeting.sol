0ce2b428646f30183bb7223dbbd22c05ccb42a85
Posted Go <110789892+postedgo@users.noreply.github.com>
2022-11-01 02:47:32 -0400

    Update Greeting.sol

--- Greeting.sol
+++ Greeting.sol
@@ -23,3 +23,14 @@ contract Greeting {
         return string(abi.encodePacked(greetingPrefix, name));
     }
 }
+- - - - - - - - - - - - 
+// solidity function definition:
+// function <function_name>(param type, param name, state mutability, returns, return type),
+//example:
+// function getGreeting()-function name public-visibility view-state mutability returns (string memory - return type) 
+//visbility specificiers, most secure to least secure
+// private: only visible in current contract
+// internal: can be called by current contract or any derived contracts
+// external: only visble externally, howver can be accessed within the current contract via 'this.func'
+
+// public - visible externally and internally - including getter functions for storage/state variables
